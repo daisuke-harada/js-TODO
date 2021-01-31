6ca51553033c8d87c7c846087a5897e53795891d
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2021-01-31 21:17:26 +0900

    [add]削除機能を実装

--- src/index.js
+++ src/index.js
@@ -23,7 +23,11 @@ const onClickAdd = () => {
   //　削除機能作成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  deleteButton.addEventListener("click", () => {});
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   // divタグの子要素に各要素を設定
   div.appendChild(li);
